1105c2900927ef060bfd5045b22f3dcf6a2ae4b3
Mikhail <gin371@yandex.ru>
2020-03-16 12:27:12 +0300

    fix natasha

--- src/getcontact/natasha_name.py
+++ src/getcontact/natasha_name.py
@@ -15,23 +15,5 @@ def name_max(text):
     max_name = max(counter.items(), key=operator.itemgetter(1))[0]
     return max_name
 
-text = '''
-    Андрей Тимофеев
-    Андрей Спб
-    Андрей Челентос
-    Андрей Катин
-    Андрей
-     Онлрей
-     Экс Бойфренд Aka Реальный Долбоеб
-     Андрей Chelentos
-     Andrey Tymofeev
-     Андрей Тим
-     Андрюша :
-     Андрей 💑
-     .andrey
-     Andrey
-'''
-test=name_max(text)
-print(test)
-#
+
 
